6ab96c063ef83bcaa905ce4ce5a4b27e5a52d5a2
Shu Sian Lyu <shusianlyu@gmail.com>
2023-06-18 00:26:17 -0700

    Send emails using the form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,12 +1,19 @@
 import streamlit as st
-import flask
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="contact_form"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message")
-    button = st.form_submit_button()
+    raw_message = st.text_area("Your message")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+"""
+    button = st.form_submit_button("Submit")
     if button:
-        message = message + user_email
+        send_email(message)
+        st.info("Your message is sent successfully!")
 
