fe981e8856f86a70664ec737a60600662e606a44
Shu Sian Lyu <shusianlyu@gmail.com>
2023-06-17 22:03:39 -0700

    Add app description, image, and links

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    original_img = Image.open("images/photo.png")
-    st.image(original_img)
+    img = Image.open("images/photo.png")
+    st.image(img)
 
 with col2:
     st.title("Jessie Lyu")
@@ -34,14 +34,20 @@ Feel free to contact me!
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 data = pd.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in data[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in data[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
